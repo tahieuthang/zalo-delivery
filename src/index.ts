54670e2ff58a63f9fdc47f3914ac1856707baac3
Thang <tahieuthang@gmail.com>
2026-05-28 18:02:33 +0700

    first commit

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req: Request, res: Response) => {
+  res.json({ message: 'Welcome to ZaloDelivery Express API!' });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
