1a08be4429ed60e3e26a31c655ce91ebfa9bf941
Kamadee <tahieuthang@gmail.com>
2026-06-10 23:18:15 +0700

    fix: calculate tracking

--- src/modules/tracking/tracking.service.ts
+++ src/modules/tracking/tracking.service.ts
@@ -17,9 +17,9 @@ export function getHaversineDistance(lat1: number, lon1: number, lat2: number, l
   const a =
     Math.sin(dLat / 2) * Math.sin(dLat / 2) +
     Math.cos((lat1 * Math.PI) / 180) *
-      Math.cos((lat2 * Math.PI) / 180) *
-      Math.sin(dLon / 2) *
-      Math.sin(dLon / 2);
+    Math.cos((lat2 * Math.PI) / 180) *
+    Math.sin(dLon / 2) *
+    Math.sin(dLon / 2);
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
   return R * c;
 }
